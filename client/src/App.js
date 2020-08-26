d9e6dd8ac0326403ad19186334a2dd647e58351f
felixf3600 <felixf3600@gmail.com>
2020-08-25 22:54:25 -0700

    added warehouse-detail files and directories. added react to jsx files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+import "./Sass/App.css";
+import WarehouseDetails from "./components/WarehouseDetail";
+import WarehouseInfo from "./components/WarehouseInfo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    inventory: [],
+    products: [],
+    warehouse: [],
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="instock">
+        <Switch>
+          <Route
+            path="/warehouse/:id"
+            render={() => (
+              <WarehouseDetails
+                warehouseItems={this.state.inventory}
+                WarehouseInfo={this.state.warehouse}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ client/src/components/WarehouseDetail.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import WarehouseItem from "./WarehouseItem.jsx";
+import WarehouseHeader from ".WarehouseHeader.jsk";
+import WarehouseInfo from "./WarehouseInfo.jsx";
+import sortIcon from "../assets/icons/sort-24px.svg";
+
+function WarehouseDetail(warehouseItems, warehouseInfo) {
+  const warehouseList = warehouseItems.map((item) => {
+    if (item.warehouseID === warehouseInfo.id)
+      <WarehouseItem key={item.name} item={item} />;
+  });
+
+  return (
+    <div className="warehouse-detail">
+      <div className="warehouse-detail__container">
+        <WarehouseHeader name={warehouseInfo.name} />
+        <WarehouseInfo warehouseInfo={warehouseInfo} />
+        <div className="warehouse-detail__label">
+          <h4 className="warehouse-detail__label-item">
+            INVENTORY ITEM {sortIcon}
+          </h4>
+          <h4 className="warehouse-detail__label-category">
+            CATEGORY {sortIcon}
+          </h4>
+          <h4 className="warehouse-detail__label-status">STATUS {sortIcon}</h4>
+          <h4 className="warehouse-detail__label-qty">QUANTITY {sortIcon}</h4>
+          <h4 className="warehouse-detail__label-actions">ACTIONS</h4>
+        </div>
+        <ul className="warehouse-detail__list">{warehouseList}</ul>
+      </div>
+    </div>
+  );
+}
+
+export default WarehouseDetail;
