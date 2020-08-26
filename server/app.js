2cc130116e7c06d7b018025a4cc4292714b82653
unknown <nafis1030@gmail.com>
2020-08-26 01:22:00 -0400

    Adding structure for inventories to the backend, and also updated the structure for warehouse

--- server/app.js
+++ server/app.js
@@ -4,14 +4,16 @@ const cors = require("cors");
 require("dotenv").config();
 const { PORT, BACKEND_URL } = process.env;
 const warehouseRoute = require("./routes/warehouseRoute");
+const inventoriesRoute = require("./routes/inventoriesRoute");
 
 // add middleware to help work with req.body
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-// Warehouse endpoint, setup using express.Router()
+// Warehouse, inventory endpoint, setup using express.Router()
 app.use("/warehouse", warehouseRoute);
+app.use("/inventories", inventoriesRoute);
 
 // listen, start the application
 app.listen(PORT, () => console.log(`listening at: ${BACKEND_URL}:${PORT}`));
