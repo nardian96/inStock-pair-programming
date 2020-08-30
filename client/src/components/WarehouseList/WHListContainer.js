84d085cb9b79741b988ebc769440245cfebf0f59
Nadia <nadia.ardian96@gmail.com>
2020-08-30 01:07:35 -0700

    Tablet and Desktop responsive design for warehouse list done

--- client/src/components/WarehouseList/WHListContainer.js
+++ client/src/components/WarehouseList/WHListContainer.js
@@ -6,23 +6,25 @@ function WHListContainer(props) {
     return (
         <>
         <div className="list__main-header">
-            <div className="list__main-subheader">
+            <div className="list__main-subheader list__main-subheader--one">
                 <h4>WAREHOUSE</h4>
                 <img src={sortIcon}/>
             </div>
-            <div className="list__main-subheader">
+            <div className="list__main-subheader list__main-subheader--two">
                 <h4>ADDRESS</h4>
                 <img src={sortIcon}/>
             </div>
-            <div className="list__main-subheader">
+            <div className="list__main-subheader list__main-subheader--three">
                 <h4>CONTACT NAME</h4>
                 <img src={sortIcon}/>
             </div>
-            <div className="list__main-subheader">
+            <div className="list__main-subheader list__main-subheader--four">
                 <h4>CONTACT INFORMATION</h4>
                 <img src={sortIcon}/>
             </div>
-            <h4>ACTIONS</h4>
+            <div className="list__main-subheader list__main-subheader--five">
+                <h4>ACTIONS</h4>
+            </div>
         </div>
         <ul className="warehouse__list-container">
             {props.warehouses
